201f8b0ee327bfc8d030136980ef17a057653bb4
Grazziano Fagundes <grazzianofagundes@gmail.com>
2020-01-10 17:38:03 -0300

    Adding our player buttons

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 include("includes/config.php");
 // session_destroy();
 
-if (isset($_SESSION['userLoggedIn'])) {
-    $userLoggedIn = $_SESSION['userLoggedIn'];
-} else {
-    header("Location: register.php");
-}
+// if (isset($_SESSION['userLoggedIn'])) {
+//     $userLoggedIn = $_SESSION['userLoggedIn'];
+// } else {
+//     header("Location: register.php");
+// }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -29,7 +29,35 @@ if (isset($_SESSION['userLoggedIn'])) {
             </div>
 
             <div id="nowPlayingCenter">
+                <div class="content playerControls">
+                    <div class="buttons">
+                        
+                        <button class="controlButton shuffle" title="Shuffle Button">
+                            <img src="assets/images/icons/shuffle.png" alt="Shuffle">
+                        </button>
 
+                        <button class="controlButton previous" title="Previous Button">
+                            <img src="assets/images/icons/previous.png" alt="Previous">
+                        </button>
+
+                        <button class="controlButton play" title="Play Button">
+                            <img src="assets/images/icons/play.png" alt="Play">
+                        </button>
+
+                        <button class="controlButton pause" title="Pause Button" style="display: none">
+                            <img src="assets/images/icons/pause.png" alt="Pause">
+                        </button>
+
+                        <button class="controlButton next" title="Next Button">
+                            <img src="assets/images/icons/next.png" alt="Next">
+                        </button>
+
+                        <button class="controlButton repeat" title="Repeat Button">
+                            <img src="assets/images/icons/repeat.png" alt="Repeat">
+                        </button>
+
+                    </div>
+                </div>
             </div>
 
             <div id="nowPlayingRight">
