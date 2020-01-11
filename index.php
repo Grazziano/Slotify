db0eaa3bd0992713874bb65a960405e9ae5d66fd
Grazziano Fagundes <grazzianofagundes@gmail.com>
2020-01-11 20:34:51 -0300

    Navigation bar site logo

--- index.php
+++ index.php
@@ -26,7 +26,11 @@ if (isset($_SESSION['userLoggedIn'])) {
         <div id="topContainer">
 
             <div id="navBarContainer">
-            
+                <nav class="navBar">
+                    <a href="index.php" class="logo">
+                        <img src="assets/images/icons/logo.png" alt="">
+                    </a>
+                </nav>
             </div>
 
         </div>
