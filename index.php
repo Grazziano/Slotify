d67d52e5bb6e5af8044d50b4718b854e5e09820f
Grazziano Fagundes <grazzianofagundes@gmail.com>
2020-01-11 23:17:20 -0300

    Outputting the album artwork images

--- index.php
+++ index.php
@@ -4,10 +4,16 @@
 
 <div class="gridViewContainer">
     <?php
-    $albumQuery = mysqli_query($con, "SELECT * FROM albums");
+    $albumQuery = mysqli_query($con, "SELECT * FROM albums ORDER BY RAND() LIMIT 10");
 
     while ($row = mysqli_fetch_array($albumQuery)) {
-        echo $row['title'] . "<br>";
+
+        echo "<div clas-'gridViewItem'>
+                <img src='" . $row['artworkPath'] . "'>
+                <div class='gridViewInfo'>
+                    " . $row['title'] . "
+                </div>      
+              </div>";
     }
     ?>
 </div>
