e418ff79deee7891218ff85442d8f4e7b2e01260
Grazziano Fagundes <grazzianofagundes@gmail.com>
2020-01-16 11:49:07 -0300

    Replacing all links with our dynamic links

--- album.php
+++ album.php
@@ -1,4 +1,5 @@
-<?php include("includes/header.php");
+<?php
+include("includes/includedFiles.php");
 
 if (isset($_GET['id'])) {
     $albumId = $_GET['id'];
@@ -69,5 +70,3 @@ $artist = $album->getArtist();
 
     </ul>
 </div>
-
-<?php include("includes/footer.php"); ?>
