d86027427d36b7835771429757b1d70333be6539
Grazziano Fagundes <grazzianofagundes@gmail.com>
2020-02-03 00:06:18 -0300

    The createPlaylist function

--- yourMusic.php
+++ yourMusic.php
@@ -5,7 +5,7 @@ include("includes/includedFiles.php");
     <div class="gridViewContainer">
         <h2>PLAYLISTS</h2>
         <div class="buttonItems">
-            <button class="button green">NEW PLAYLIST</button>
+            <button class="button green" onclick="createPlaylist('sagsh')">NEW PLAYLIST</button>
         </div>
     </div>
 </div>
