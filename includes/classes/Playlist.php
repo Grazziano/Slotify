57bd34a616dee96e4004a51a0caf6337cf8e2c1d
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2021-04-24 00:23:26 -0300

    Populating the dropdown with playlists

--- includes/classes/Playlist.php
+++ includes/classes/Playlist.php
@@ -54,8 +54,15 @@ class Playlist
     public static function getPlaylistsDropdown($con, $username)
     {
         $dropdown = '<select class="item playlist" name="" id="">
-                        <option value="">Add to playlist</option>
-                    </select>';
-        return $dropdown;
+                        <option value="">Add to playlist</option>';
+        $query = mysqli_query($con, "SELECT id, name FROM playlists WHERE owner = '$username'");
+
+        while ($row = mysqli_fetch_array($query)) {
+            $id = $row['id'];
+            $name = $row['name'];
+            $dropdown = $dropdown . "<option value='$id'>$name</option>";
+        }
+
+        return $dropdown . "</select>";
     }
 }
