d60490ff415f4b3615349dbb01c981c2e5ab3f5a
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2021-04-23 22:45:35 -0300

    Creating the options menu

--- yourMusic.php
+++ yourMusic.php
@@ -1,16 +1,20 @@
 <?php
 include("includes/includedFiles.php");
 ?>
+
 <div class="playlistsContainer">
+
     <div class="gridViewContainer">
         <h2>PLAYLISTS</h2>
+
         <div class="buttonItems">
             <button class="button green" onclick="createPlaylist()">NEW PLAYLIST</button>
         </div>
 
         <?php
         $username = $userLoggedIn->getUsername();
-        $playlistsQuery = mysqli_query($con, "SELECT * FROM playlists WHERE owner = '$username'");
+
+        $playlistsQuery = mysqli_query($con, "SELECT * FROM playlists WHERE owner='$username'");
 
         if (mysqli_num_rows($playlistsQuery) == 0) {
             echo "<span class='noResults'>You don't have any playlists yet.</span>";
@@ -20,12 +24,15 @@ include("includes/includedFiles.php");
 
             $playlist = new Playlist($con, $row);
 
-            echo "<div class='gridViewInfo' role='link' tabindex='0' onClick='openPage(\"playlist.php?id=" . $playlist->getId() . "\")'>
-                    <div class='playlistImage'>
-                        <img src='assets/images/icons/playlist.png'></img>
-                    </div>
-                    " . $playlist->getName() . "
-                    </div>";
+            echo "<div class='gridViewItem' role='link' tabindex='0' 
+							onclick='openPage(\"playlist.php?id=" . $playlist->getId() . "\")'>
+						<div class='playlistImage'>
+							<img src='assets/images/icons/playlist.png'>
+						</div>						
+						<div class='gridViewInfo'>"
+                . $playlist->getName() .
+                "</div>
+                </div>";
         }
         ?>
 
