fac6c3adaa99743f0fb246de0059e0598b9981f5
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2021-04-24 20:24:31 -0300

    Removing song ajax code

--- playlist.php
+++ playlist.php
@@ -78,4 +78,5 @@ $owner = new User($con, $playlist->getOwner());
 <nav class="optionsMenu">
     <input type="hidden" class="songId" name="">
     <?php echo Playlist::getPlaylistsDropdown($con, $userLoggedIn->getUsername()); ?>
+    <div class="item" onclick="removeFromPlaylist(this, '<?php echo $playlistId; ?>')">Remove From Playlist</div>
 </nav>
