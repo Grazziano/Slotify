d08f8124bd0f3019f5967f0dd4950980608800bf
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2021-04-24 22:03:39 -0300

    User details form

--- updateDetails.php
+++ updateDetails.php
@@ -1,1 +1,19 @@
-<h1>Olá</h1>
+<?php
+include("includes/includedFiles.php");
+?>
+<div class="userDetails">
+    <div class="container borderBotton">
+        <h2>EMAIL</h2>
+        <input type="text" class="email" name="email" placeholder="Email address..." value="<?php echo $userLoggedIn->getEmail(); ?>">
+        <span class="message"></span>
+        <button class="button" onclick="">SAVE</button>
+    </div>
+    <div class="container">
+        <h2>PASSWORD</h2>
+        <input type="text" class="oldPassword" name="oldPassword" placeholder="Current password...">
+        <input type="text" class="newPassword1" name="newPassword1" placeholder="New password...">
+        <input type="text" class="newPassword2" name="newPassword2" placeholder="Confirm password...">
+        <span class="message"></span>
+        <button class="button" onclick="">SAVE</button>
+    </div>
+</div>
