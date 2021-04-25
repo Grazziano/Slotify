e3516e101945302fcd08098084cb2e447c1d0b5b
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2021-04-24 22:13:05 -0300

    Styling the user details form

--- updateDetails.php
+++ updateDetails.php
@@ -11,8 +11,8 @@ include("includes/includedFiles.php");
     <div class="container">
         <h2>PASSWORD</h2>
         <input type="text" class="oldPassword" name="oldPassword" placeholder="Current password...">
-        <input type="text" class="newPassword1" name="newPassword1" placeholder="New password...">
-        <input type="text" class="newPassword2" name="newPassword2" placeholder="Confirm password...">
+        <input type="password" class="newPassword1" name="newPassword1" placeholder="New password...">
+        <input type="password" class="newPassword2" name="newPassword2" placeholder="Confirm password...">
         <span class="message"></span>
         <button class="button" onclick="">SAVE</button>
     </div>
