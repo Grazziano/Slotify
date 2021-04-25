f7c4293abdafc4c69d6ea384ffb871dd830198be
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2021-04-24 23:00:47 -0300

    Fix js

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,29 +23,30 @@ $(window).scroll(function () {
 
 $(document).on("change", "select.playlist", function () {
     var select = $(this);
-    var playlistId = $(this).val();
+    var playlistId = select.val();
     var songId = select.prev(".songId").val();
 
-    // console.log("playlistId" + playlistId);
-    // console.log("songId" + songId);
-    $.post("includes/handlers/ajax/addToPlaylist.php", { playlistId: playlistId, songId: songId }).done(function (error) {
+    $.post("includes/handlers/ajax/addToPlaylist.php", { playlistId: playlistId, songId: songId })
+        .done(function (error) {
 
-        if (error != "") {
-            alert(error);
-            return;
-        }
+            if (error != "") {
+                alert(error);
+                return;
+            }
 
-        hideOptionsMenu();
-        select.val("");
-    });
+            hideOptionsMenu();
+            select.val("");
+        });
 });
+
 
 function updateEmail(emailClass) {
     var emailValue = $("." + emailClass).val();
 
-    $.post("includes/handlers/ajax/updateEmail.php", { email: emailValue, username: userLoggedIn }).done(function (response) {
-        $("." + emailClass).nextAll(".message").text(response);
-    });
+    $.post("includes/handlers/ajax/updateEmail.php", { email: emailValue, username: userLoggedIn })
+        .done(function (response) {
+            $("." + emailClass).nextAll(".message").text(response);
+        })
 }
 
 function updatePassword(oldPasswordClass, newPasswordClass1, newPasswordClass2) {
@@ -81,10 +82,8 @@ function openPage(url) {
     if (url.indexOf("?") == -1) {
         url = url + "?";
     }
-
-    var encodeUrl = encodeURI(url + "&userLoggedIn=" + userLoggedIn);
-    // console.log(encodeUrl);
-    $("#mainContent").load(encodeUrl);
+    var encodedUrl = encodeURI(url + "&userLoggedIn=" + userLoggedIn);
+    $("#mainContent").load(encodedUrl);
     $("body").scrollTop(0);
     history.pushState(null, null, url);
 }
@@ -92,46 +91,51 @@ function openPage(url) {
 function removeFromPlaylist(button, playlistId) {
     var songId = $(button).prevAll(".songId").val();
 
-    $.post("includes/handlers/ajax/removeFromPlaylist.php", { playlistId: playlistId, songId: songId }).done(function (error) {
-        if (error != "") {
-            alert(error);
-            return;
-        }
-        // do something when ajax returns
-        openPage("playlist.php?id=" + playlistId);
-    });
+    $.post("includes/handlers/ajax/removeFromPlaylist.php", { playlistId: playlistId, songId: songId })
+        .done(function (error) {
+
+            if (error != "") {
+                alert(error);
+                return;
+            }
+            //do something when ajax returns
+            openPage("playlist.php?id=" + playlistId);
+        });
 }
 
 function createPlaylist() {
-    console.log(userLoggedIn);
+
     var popup = prompt("Please enter the name of your playlist");
 
     if (popup != null) {
-        // console.log(alert);
+
         $.post("includes/handlers/ajax/createPlaylist.php", { name: popup, username: userLoggedIn })
             .done(function (error) {
+
                 if (error != "") {
                     alert(error);
                     return;
                 }
-                // do something when ajax returns
+
+                //do something when ajax returns
                 openPage("yourMusic.php");
             });
     }
 }
 
 function deletePlaylist(playlistId) {
-    var prompt = confirm("Are you sure you want to delete this playlist?");
+    var prompt = confirm("Are you sure you want to delte this playlist?");
 
     if (prompt == true) {
-        // console.log("DELETE PLAYLIST");
+
         $.post("includes/handlers/ajax/deletePlaylist.php", { playlistId: playlistId })
             .done(function (error) {
+
                 if (error != "") {
                     alert(error);
                     return;
                 }
-                // do something when ajax returns
+                //do something when ajax returns
                 openPage("yourMusic.php");
             });
     }
@@ -139,7 +143,6 @@ function deletePlaylist(playlistId) {
 
 function hideOptionsMenu() {
     var menu = $(".optionsMenu");
-
     if (menu.css("display") != "none") {
         menu.css("display", "none");
     }
@@ -151,8 +154,8 @@ function showOptionsMenu(button) {
     var menuWidth = menu.width();
     menu.find(".songId").val(songId);
 
-    var scrollTop = $(window).scrollTop(); // Distance from top of window to top of document
-    var elementOffset = $(button).offset().top; // Distance from top of document
+    var scrollTop = $(window).scrollTop(); //Distance from top of window to top of document
+    var elementOffset = $(button).offset().top; //Distance from top of document
 
     var top = elementOffset - scrollTop;
     var left = $(button).position().left;
